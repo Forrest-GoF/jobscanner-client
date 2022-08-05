f75d3153b3c9022c7e9e4cc94338641f2c034c3c
youngmin wi <youngminieo1005@gmail.com>
2022-08-05 23:55:35 +0900

    [GOF-103]:bento: Spoqa Han Sans 폰트 추가

--- src/styles/GlobalStyle.tsx
+++ src/styles/GlobalStyle.tsx
@@ -6,9 +6,53 @@ const GlobalStyle = () => {
 	return (
 		<Global
 			styles={css`
-				${reset}
+				${reset};
 
-				ul, li {
+				@font-face {
+					font-family: 'Spoqa_Han_Sans';
+					font-weight: 700;
+					src: local(''), url('/fonts/Spoqa_Han_Sans_Bold.woff2') format('woff2'),
+						url('/fonts/Spoqa_Han_Sans_Bold.woff') format('woff'),
+						url('/fonts/Spoqa_Han_Sans_Bold.ttf') format('truetype');
+				}
+
+				@font-face {
+					font-family: 'Spoqa_Han_Sans';
+					font-weight: 400;
+					src: local(''), url('/fonts/Spoqa_Han_Sans_Regular.woff2') format('woff2'),
+						url('/fonts/Spoqa_Han_Sans_Regular.woff') format('woff'),
+						url('/fonts/Spoqa_Han_Sans_Regular.ttf') format('truetype');
+				}
+
+				@font-face {
+					font-family: 'Spoqa_Han_Sans';
+					font-weight: 300;
+					src: local(''), url('/fonts/Spoqa_Han_Sans_Light.woff2') format('woff2'),
+						url('/fonts/Spoqa_Han_Sans_Light.woff') format('woff'),
+						url('/fonts/Spoqa_Han_Sans_Light.ttf') format('truetype');
+				}
+
+				@font-face {
+					font-family: 'Spoqa_Han_Sans';
+					font-weight: 100;
+					src: local(''), url('/fonts/Spoqa_Han_Sans_Thin.woff2') format('woff2'),
+						url('/fonts/Spoqa_Han_Sans_Thin.woff') format('woff'),
+						url('/fonts/Spoqa_Han_Sans_Thin.ttf') format('truetype');
+				}
+
+				*,
+				*::before,
+				*::after {
+					box-sizing: border-box;
+					font-family: 'Spoqa_Han_Sans', sans-serif;
+				}
+
+				body {
+					font-family: 'Spoqa_Han_Sans', sans-serif;
+				}
+
+				ul,
+				li {
 					list-style: none;
 				}
 			`}
