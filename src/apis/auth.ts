f9efb561ff9df9a85e4c45f47e183711536d4f40
youngmin wi <youngminieo1005@gmail.com>
2022-08-10 16:09:22 +0900

    [GOF-103]:sparkles: 로그아웃 플로우 구현(API 연결 X)

--- src/apis/auth.ts
+++ src/apis/auth.ts
@@ -1,6 +1,6 @@
 import { PATH, POST } from '@/constants/api';
 import { requester } from '@/apis/requester';
-import { KEYS, setLocalStorageItem } from '@/utils/storage';
+import { KEYS, setLocalStorageItem, removeLocalStorageItem } from '@/utils/storage';
 import { LoginResponse } from '@/types/apis/login';
 
 export const kakaoLogin = async (kakaoAccessToken: string) => {
@@ -12,19 +12,21 @@ export const kakaoLogin = async (kakaoAccessToken: string) => {
 		const { headers, data, status } = await requester<LoginResponse>({
 			method: POST,
 			url: kakao,
-			headers: {
+			data: {
 				accessToken: kakaoAccessToken,
 			},
+			// headers: {
+			// 	accessToken: kakaoAccessToken,
+			// },
 		});
 
 		// const JOB_SCANNER_ACCESS_TOKEN = headers.authorization.split(' ')[1];
 		// setLocalStorageItem(KEYS.JOB_SCANNER_ACCESS_TOKEN, JOB_SCANNER_ACCESS_TOKEN);
-		return status;
+		// return status;
 
-		// console.log(response);
-		// const JOB_SCANNER_ACCESS_TOKEN = response.data?.appToken;
-		// setLocalStorageItem(KEYS.JOB_SCANNER_ACCESS_TOKEN, JOB_SCANNER_ACCESS_TOKEN);
-		// return response.status;
+		const JOB_SCANNER_ACCESS_TOKEN = data?.appToken;
+		setLocalStorageItem(KEYS.JOB_SCANNER_ACCESS_TOKEN, JOB_SCANNER_ACCESS_TOKEN);
+		return status;
 	} catch (error) {
 		console.log(`카카오 로그인 에러: ${error}`);
 	}
@@ -35,10 +37,11 @@ export const logout = async () => {
 		auth: { logout },
 	} = PATH;
 
-	const response = await requester({
-		method: POST,
-		url: logout,
-	});
+	// const response = await requester({
+	// 	method: POST,
+	// 	url: logout,
+	// });
 
-	// removeLocalStorageItem(KEYS.JOB_SCANNER_ACCESS_TOKEN);
+	removeLocalStorageItem(KEYS.JOB_SCANNER_ACCESS_TOKEN);
+	// return response;
 };
