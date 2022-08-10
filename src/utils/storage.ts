4fef2becad446e19f06183e5a11a4a73c2f35c24
youngmin wi <youngminieo1005@gmail.com>
2022-08-10 15:36:52 +0900

    [GOF-103]:seedling: Storage Util 파일 추가

---
+++ src/utils/storage.ts
@@ -0,0 +1,30 @@
+const EMPTY_VALUE = '';
+
+export const KEYS = {
+	JOB_SCANNER_ACCESS_TOKEN: 'JOB_SCANNER_ACCESS_TOKEN',
+};
+
+export const getLocalStorageItem = (key: string): any => {
+	try {
+		const item = localStorage.getItem(key);
+		return item ? JSON.parse(item) : EMPTY_VALUE;
+	} catch (error) {
+		throw new Error(`로컬 스토리지 데이터 저장 에러: ${error}`);
+	}
+};
+
+export const setLocalStorageItem = (key: string, value: any) => {
+	try {
+		localStorage.setItem(key, JSON.stringify(value));
+	} catch (error) {
+		throw new Error(`로컬 스토리지 값 저장 에러: ${error}`);
+	}
+};
+
+export const removeLocalStorageItem = (key: string) => {
+	try {
+		localStorage.removeItem(key);
+	} catch (error) {
+		throw new Error(`로컬 스토리지 값 삭제 에러: ${error}`);
+	}
+};
