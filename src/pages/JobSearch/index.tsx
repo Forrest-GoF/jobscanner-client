c4ca31ad144c4b91ddb943a23f6160d77e2ba350
youngmin wi <youngminieo1005@gmail.com>
2022-08-11 22:14:40 +0900

    [GOF-130]:adhesive_bandage: JobSearch 컴포넌트에 Navbar UI 테스트용 삽입

--- src/pages/JobSearch/index.tsx
+++ src/pages/JobSearch/index.tsx
@@ -1,7 +1,7 @@
 import { Link } from 'react-router-dom';
 import { useQuery, useMutation } from '@tanstack/react-query';
 import * as S from './styled';
-import SideBar from '@/components/features/Sidebar';
+import { Sidebar, Navbar } from '@/components/features';
 import { getJobs } from '@/apis/job';
 
 const JobSearch = () => {
@@ -9,8 +9,9 @@ const JobSearch = () => {
 
 	return (
 		<S.Container>
-			<SideBar />
+			<Sidebar />
 			<S.InnerWrapper>
+				<Navbar />
 				공고 리스트 페이지입니다.
 				<Link to="/jobs/1">공고 상세리스트 페이지로 이동</Link>
 			</S.InnerWrapper>
