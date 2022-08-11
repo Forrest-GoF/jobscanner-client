35e2637c1b0fab2d327e9d3c90c26037c96040b1
youngmin wi <youngminieo1005@gmail.com>
2022-08-11 20:18:28 +0900

    [GOF-130]:lipstick: Sidebar UI 추가

--- src/pages/JobSearch/index.tsx
+++ src/pages/JobSearch/index.tsx
@@ -1,19 +1,20 @@
-import React, { useEffect } from 'react';
 import { Link } from 'react-router-dom';
-import { getJobs } from '@/apis/job';
 import { useQuery, useMutation } from '@tanstack/react-query';
-import axios from 'axios';
-import { logout } from '@/apis/auth';
-import { getLocalStorageItem, KEYS, removeLocalStorageItem } from '@/utils/storage';
+import * as S from './styled';
+import SideBar from '@/components/features/Sidebar';
+import { getJobs } from '@/apis/job';
 
 const JobSearch = () => {
-	const { data: jobs } = useQuery(['getJobs'], getJobs);
+	// const { data: jobs } = useQuery(['getJobs'], getJobs);
 
 	return (
-		<div>
-			공고 리스트 페이지입니다.
-			<Link to="/jobs/1">공고 상세리스트 페이지로 이동</Link>
-		</div>
+		<S.Container>
+			<SideBar />
+			<S.InnerWrapper>
+				공고 리스트 페이지입니다.
+				<Link to="/jobs/1">공고 상세리스트 페이지로 이동</Link>
+			</S.InnerWrapper>
+		</S.Container>
 	);
 };
 
