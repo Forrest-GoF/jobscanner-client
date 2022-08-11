1773c3f307aa2312e3fceb2925194a477a2ca612
youngmin wi <youngminieo1005@gmail.com>
2022-08-11 20:16:02 +0900

    [GOF-130]:lipstick: Emotion Theme 추가

--- src/types/emotion.d.ts
+++ src/types/emotion.d.ts
@@ -23,6 +23,52 @@ declare module '@emotion/react' {
 				700: string;
 				800: string;
 			};
+			grey: {
+				100: string;
+				200: string;
+				300: string;
+				400: string;
+				500: string;
+				600: string;
+				700: string;
+				800: string;
+			};
+			white: string;
+			black: string;
+		};
+		fontSize: {
+			fs10: string;
+			fs11: string;
+			fs12: string;
+			fs13: string;
+			fs14: string;
+			fs15: string;
+			fs16: string;
+			fs17: string;
+			fs18: string;
+			fs19: string;
+			fs20: string;
+		};
+
+		fontWeight: {
+			light: number;
+			regular: number;
+			medium: number;
+			bold: number;
+			strongBold: number;
+		};
+		lineHeight: {
+			lh10: string;
+			lh11: string;
+			lh12: string;
+			lh13: string;
+			lh14: string;
+			lh15: string;
+			lh16: string;
+			lh17: string;
+			lh18: string;
+			lh19: string;
+			lh20: string;
 		};
 	}
 }
