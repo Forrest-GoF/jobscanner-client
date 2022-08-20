563f04e3cb606d4d79e1ac8472b71a14aaba1f1b
youngmin wi <youngminieo1005@gmail.com>
2022-08-20 17:30:20 +0900

    [GOF-148]:lipstick: Navbar 컴포넌트 UI 수정 - 미제공 버튼 제거

--- src/components/features/Navbar/index.tsx
+++ src/components/features/Navbar/index.tsx
@@ -1,29 +1,27 @@
-import React from 'react';
 import * as S from './styled';
-import { MdOutlineNotificationsActive, MdLanguage, MdOutlineDarkMode } from 'react-icons/md';
-import { Avatar } from '@/components/shared';
+import { Link } from 'react-router-dom';
+import { Avatar, LinkButton } from '@/components/shared';
 
 const Navbar = () => {
 	return (
 		<S.Container>
 			<S.List>
 				<S.ListItem>
-					<MdLanguage />
-				</S.ListItem>
-				<S.ListItem>
-					<MdOutlineDarkMode />
-				</S.ListItem>
-				<S.ListItem>
-					<MdOutlineNotificationsActive />
-					<S.NotificationCounter>1</S.NotificationCounter>
-				</S.ListItem>
-				<S.ListItem>
 					<Avatar
-						size="30px"
+						size="50px"
 						imgSrc={
 							'https://images.unsplash.com/photo-1511367461989-f85a21fda167?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1031&q=80'
 						}
 					/>
+					<S.ProfileWrapper>
+						<p>강소마님 반갑습니다 👋🏻</p>
+						SW Developer
+					</S.ProfileWrapper>
+				</S.ListItem>
+				<S.ListItem>
+					<LinkButton href="/login" buttonType="primary" padding="1rem 2rem" borderRadius="0.5rem" fontSize="1rem">
+						로그인
+					</LinkButton>
 				</S.ListItem>
 			</S.List>
 		</S.Container>
