4ec8f5e4c9b929cdd497fd4e4d3824d053596406
youngmin wi <youngminieo1005@gmail.com>
2022-08-20 17:28:29 +0900

    [GOF-148]:adhesive_bandage: 공용 링크 버튼 Props 확장

--- src/components/shared/LinkButton/index.tsx
+++ src/components/shared/LinkButton/index.tsx
@@ -1,10 +1,10 @@
 import * as S from './styled';
 import { Props } from './type';
 
-const LinkButton = ({ href, target, rel, children, ...props }: Props) => {
+const LinkButton = ({ href, target, rel, padding, children, ...props }: Props) => {
 	return (
 		<S.Container {...props}>
-			<S.LinkButton href={href} target={target} rel={rel}>
+			<S.LinkButton href={href} target={target} rel={rel} padding={padding}>
 				{children}
 			</S.LinkButton>
 		</S.Container>
