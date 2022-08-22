6a8315686466c88f4c3adecb62fcb5b8bc0f9333
youngmin wi <youngminieo1005@gmail.com>
2022-08-23 03:39:03 +0900

    [GOF-148]:sparkles: 공고 목록, 공고 상세  API 데이터 연결

--- src/pages/JobSearch/index.tsx
+++ src/pages/JobSearch/index.tsx
@@ -1,11 +1,14 @@
 import Layout from '@/components/layout';
 import { JobList, JobSearchForm } from '@/components/features';
-
+import { useQuery } from '@tanstack/react-query';
+import { getJobs } from '@/apis/job';
 const JobSearch = () => {
+	const { data: jobs } = useQuery(['getJobs'], getJobs);
+
 	return (
 		<Layout>
 			{/* <JobSearchForm /> */}
-			<JobList />
+			<JobList jobs={jobs} />
 		</Layout>
 	);
 };
