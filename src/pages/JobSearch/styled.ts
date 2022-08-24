7c7131a31bf5754f34969413243e409906428c70
youngmin wi <youngminieo1005@gmail.com>
2022-08-25 05:12:59 +0900

    [GOF-139]:lipstick: 무한 스크롤 로딩 컴포넌트 UI 추가

--- src/pages/JobSearch/styled.ts
+++ src/pages/JobSearch/styled.ts
@@ -1,7 +1,24 @@
+import { keyframes } from '@emotion/react';
 import styled from '@emotion/styled';
+
+const spin = keyframes`
+  100% {
+    transform: translate3d(0, -8px , 0);
+  }
+`;
 
 export const JobListContainer = styled.div`
 	max-height: calc(100% - 240px);
 	padding: 16px;
 	overflow-y: auto; ;
+`;
+
+export const LoadingWrapper = styled.div`
+	display: flex;
+	justify-content: center;
+
+	& > img {
+		max-width: 4em;
+		animation: ${spin} 0.3s infinite alternate linear;
+	}
 `;
