2e1a4bcf7cfc8a66203d2d18ec024fe0ae7815f4
youngmin wi <youngminieo1005@gmail.com>
2022-09-02 02:48:06 +0900

    [GOF-151]:sparkles: 소셜 로그인 인가 코드 발급 공통 로직 분리

--- src/apis/auth.ts
+++ src/apis/auth.ts
@@ -1,46 +1,22 @@
 import { PATH, POST } from '@/constants/api';
 import { requester } from '@/apis/requester';
-import { KEYS, setLocalStorageItem, removeLocalStorageItem } from '@/utils/storage';
+import { KEYS, removeLocalStorageItem } from '@/utils/storage';
 import { SingInResponse } from '@/types/apis/login';
 
-export const kakaoSignup = async (kakaoAccessToken: string) => {
+export const socialLogin = async (authorizationCode: string, social?: string) => {
 	const {
-		auth: { index, signup, kakao },
+		auth: { index, signin },
 	} = PATH;
 
-	const { headers, data, status } = await requester({
+	const { headers, data, status } = await requester<SingInResponse>({
 		method: POST,
-		url: `${index}${signup}${kakao}`,
+		url: `${index}${signin}${social && `/${social}`}`,
 		headers: {
-			Authorization: `Bearer ${kakaoAccessToken}`,
+			authorizationCode,
 		},
 	});
 
-	return status;
-};
-
-export const kakaoSignin = async (kakaoAccessToken: string) => {
-	try {
-		const {
-			auth: { index, login, kakao },
-		} = PATH;
-
-		const { headers, data, status } = await requester<SingInResponse>({
-			method: POST,
-			url: `${index}${login}${kakao}`,
-			headers: {
-				Authorization: `Bearer ${kakaoAccessToken}`,
-			},
-		});
-
-		const JOB_STORY_ACCESS_TOKEN = data?.appToken;
-		const JOB_STORY_REFRESH_TOKEN = data?.refreshToken;
-		setLocalStorageItem(KEYS.JOB_STORY_ACCESS_TOKEN, JOB_STORY_ACCESS_TOKEN);
-		setLocalStorageItem(KEYS.JOB_STORY_REFRESH_TOKEN, JOB_STORY_REFRESH_TOKEN);
-		return { data, status };
-	} catch (error) {
-		console.log(`카카오 로그인 에러: ${error}`);
-	}
+	return { data, status };
 };
 
 export const logout = async () => {
