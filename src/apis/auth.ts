e1ea2183c7b85310048f39a4987d24352207e46a
youngmin wi <youngminieo1005@gmail.com>
2022-09-08 14:12:01 +0900

    [GOF-167]:sparkles: sentry 초기화 & 작동 확인용 auth api error catch 코드 추가

--- src/apis/auth.ts
+++ src/apis/auth.ts
@@ -2,21 +2,25 @@ import { PATH, POST } from '@/constants/api';
 import { requester } from '@/apis/requester';
 import { KEYS, removeLocalStorageItem } from '@/utils/storage';
 import { SingInResponse } from '@/types/apis/login';
+import * as Sentry from '@sentry/react';
 
 export const socialLogin = async (authorizationCode: string, social?: string) => {
 	const {
 		auth: { index, signin },
 	} = PATH;
 
-	const { headers, data, status } = await requester<SingInResponse>({
-		method: POST,
-		url: `${index}${signin}${social && `/${social}`}`,
-		headers: {
-			authorizationCode,
-		},
-	});
-
-	return { data, status };
+	try {
+		const { headers, data, status } = await requester<SingInResponse>({
+			method: POST,
+			url: `${index}${signin}${social && `/${social}`}`,
+			headers: {
+				authorizationCode,
+			},
+		});
+		return { data, status };
+	} catch (error) {
+		Sentry.captureException(error);
+	}
 };
 
 export const logout = async () => {
